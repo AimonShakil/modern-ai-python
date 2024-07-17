f05cebbd0598231ce5eb11cc5ce80e6a9c9eaccb
Aimon Shakeel <aimon.ipas@gmail.com>
2024-07-17 22:38:32 +0500

    Modern Python

---
+++ 09_Loop_iterative type_input_sys.argv/class09.py
@@ -0,0 +1,21 @@
+import sys  
+
+
+# name : str = input("what is Name? : \t" )
+# print(type(name))
+# print(f"Welcome dear User Ms/Miss {name}!") 
+
+
+# Iput from argv in console
+
+
+print("line1")
+print("line2")
+
+print(type(sys.argv))
+
+print(sys.argv) # By Default Iterative DT is List: recieves object in the form of input from console, check from ls in which drectory we are in
+# 0 Index = File name ** custom values / User Define
+
+# Method of running this file , go to terminal    
+
